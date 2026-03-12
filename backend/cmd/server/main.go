3bf0f366c9a02c51e7020388f88a569c7372bd56
love0107 <lovlesh0107@gmail.com>
2026-03-12 23:13:43 +0530

    feat: env setup, gitignore added

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/love0107/astro-mandir/db"
 	"github.com/love0107/astro-mandir/handlers"
 )
 
 func main() {
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Println(".env file nahi mila — environment variables use karega")
+	}
+
+	// Now you can access anywhere in code like this
+	// os.Getenv("PROKERALA_CLIENT_ID")
+
 	db.InitDB()
 
 	panchaangHandler := handlers.NewPanchaangHandler()
@@ -19,7 +30,9 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "AstroMandir chal raha hai! 🙏"})
+		c.JSON(200, gin.H{
+			"message": "AstroMandir chal raha hai! 🙏",
+		})
 	})
 
 	api := r.Group("/api")
@@ -31,5 +44,13 @@ func main() {
 	}
 
 	log.Println(" Server starting on :8080")
-	r.Run(":8080")
+	r.Run(":" + getPort())
+}
+
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return "8080"
+	}
+	return port
 }
--- backend/go.mod
+++ backend/go.mod
@@ -4,6 +4,7 @@ go 1.25.0
 
 require (
 	github.com/gin-gonic/gin v1.12.0
+	github.com/joho/godotenv v1.5.1
 	github.com/mattn/go-sqlite3 v1.14.34
 )
 
